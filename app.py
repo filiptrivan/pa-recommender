751c256e58ca0395a43c1130a2cf222c1056ab68
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2025-09-20 21:57:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ def test_hyperparameter_optimization():
     }), 200
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
